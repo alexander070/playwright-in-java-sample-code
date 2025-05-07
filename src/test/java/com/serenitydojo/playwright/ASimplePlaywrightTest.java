e35bbbd2d713f6905bf2108236797c063c57fd15
Alexander <oscaralexanderblanco@gmail.com>
2025-05-06 21:10:35 -0500

    test

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,15 +1,60 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
 
 public class ASimplePlaywrightTest {
+    private static Playwright playwright;
+    private static Browser browser;
+    private static  BrowserContext browserContext;
+    Page page;
+
+    @BeforeAll
+    static void setupBrowser(){
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+
+        );
+        browserContext = browser.newContext();
+
+    }
+
+    @BeforeEach
+    public void setUp(){
+        page = browser.newPage();
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        browser.close();
+        playwright.close();
+    }
 
     @Test
     void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+
     }
+
+    @Test
+    void shouldSearchByKeyword(){
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResults>0);
+
+    }
+
 }
