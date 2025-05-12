418c471b30cc8cc4d9c53220b4fa2fc28e40d37d
Alexander <oscaralexanderblanco@gmail.com>
2025-05-12 11:04:19 -0500

    test

--- src/test/java/com/serenitydojo/playwright/toolshop/fixtures/RecordsAllureScreenshots.java
+++ src/test/java/com/serenitydojo/playwright/toolshop/fixtures/RecordsAllureScreenshots.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.AfterEach;
 import java.io.ByteArrayInputStream;
 
 public interface RecordsAllureScreenshots {
-    @AfterEach
-    default void tearDown(Page page) {
-        recordScreenshot(page,"End of Test Screenshot");
-    }
-
-    default void recordScreenshot(Page page, String screenshotName) {
-        Allure.addAttachment(screenshotName, new ByteArrayInputStream(page.screenshot()));
-    }
+//    @AfterEach
+//    default void tearDown(Page page) {
+//        recordScreenshot(page,"End of Test Screenshot");
+//    }
+//
+//    default void recordScreenshot(Page page, String screenshotName) {
+//        Allure.addAttachment(screenshotName, new ByteArrayInputStream(page.screenshot()));
+//    }
 }
